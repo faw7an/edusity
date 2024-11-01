a6261e62bf76dfc0a8ae818e19a5f113fc491853
Fauzan Said <fauzdasoodais@gmail.com>
2024-11-01 23:22:44 +0300

    Enhance Contact component by adding icons for email, phone, and location

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './Contact.css'
-import msg_icon from ''
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
 const Contact = () => {
   return (
     <div className='contact'>
       <div className='contact-col'>
-        <h3>Send us a message <img src='' alt='' /></h3>
+        <h3>Send us a message <img src={msg_icon} alt='' /></h3>
         <p> 
      Feel free to reach out through contact form or find our contact details below. Your feedback is important to us as we strive to improve our services to our university community.
         </p>
         <ul>
-          <li>contactEdusity@gmail.com</li>
-          <li>+1 123-323-5356</li>
-          <li>77 Massachusetts Ave, Cambridge <br /> MA 02139, United States</li>
+          <li> <img src = {mail_icon}  /> contactEdusity@gmail.com</li>
+          <li><img src = {phone_icon}  />  +1 123-323-5356</li>
+          <li><img src = {location_icon}  />  77 Massachusetts Ave, Cambridge <br /> MA 02139, United States</li>
         </ul>
       </div>
       <div className='contact-col'>
